4d22600cbaa830fab2bbfe111313743441021cd3
RingKim1 <kgh9393@naver.com>
2024-05-28 18:10:06 +0900

    fix : redux error solve

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { onAddText } from "../redux/slices/textsSlice";
 
 function TextInput() {
   const [inputValue, setInputValue] = useState("");
